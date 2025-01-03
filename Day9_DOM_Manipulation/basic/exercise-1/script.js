f859acb8c9af7c69254c821e4cb33e88e1458ab3
Lopsang Lama <lopsang.hyolmopa@gmail.com>
2025-01-03 16:05:56 +0545

    button click to change text

---
+++ Day9_DOM_Manipulation/basic/exercise-1/script.js
@@ -0,0 +1,13 @@
+// Button Click to Change Text
+// Create a button that, when clicked, changes the text of a paragraph from "Hello!" to "Goodbye!".
+const para = document.querySelector('p')
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', () => para.textContent = 'Goodbye!')
+
+// Image Toggle
+// Add an image to your webpage and a button labeled "Toggle Image." Clicking the button should hide the image if it’s visible or show it if it’s hidden.
+
+// Color Picker
+// Create three buttons labeled "Red," "Green," and "Blue." Clicking each button should change the background color of the page to the respective color.
+
